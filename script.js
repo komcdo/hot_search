4b8c300ecb2b991cccec9bd50ea2119b637e44f6
Kory <komcdo@gmail.com>
2022-12-10 23:05:20 -0800

    Version bump 1.1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter Hot Search
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Twitter Hot Search
 // @author       komcdo
 // @match        https://twitter.com/*
@@ -39,21 +39,15 @@
         .hotzearch.noAnim .staticToken{transition: none;}
         .hotzearch.noAnim .zearchTokenWrap{transition: none !important;}
         @media (prefers-color-scheme: light) {
-            .hotzearch .filterDialog {
-                background: #e6e6e6;
-            }
             .hotzearch .filterDialog select,
             .hotzearch .staticToken,
             .hotzearch .cloneToken,
             .hotzearch .hotzearch_inline .inlineToken { background: #1d9bf0; color: white; }
-            .hotzearch .staticToken .staticTokenRemove{
-                background: #fafafa;
-            }
+            .hotzearch .filterDialog { background: #e6e6e6; }
+            .hotzearch .staticToken .staticTokenRemove{ background: #fafafa; }
             .hotzearch .filterDialog input{ color:black;}
             .hotzearch .filterDialog select{ color:black;}
-        }
-
-        `;
+        }`;
     document.head.append(style);
     const closeBtn = `<svg viewBox="0 0 15 15" width="10px" height="10px" aria-hidden="true" class="r-cqee49 r-4qtqp9 r-yyyyoo r-1or9b2r r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-5soawk"><g><path d="M6.09 7.5L.04 1.46 1.46.04 7.5 6.09 13.54.04l1.42 1.42L8.91 7.5l6.05 6.04-1.42 1.42L7.5 8.91l-6.04 6.05-1.42-1.42L6.09 7.5z"></path></g></svg>`;
     const filterBtn = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" class="bi bi-funnel" viewBox="0 0 16 16"><path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"></path></svg>`;
